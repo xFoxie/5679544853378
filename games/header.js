2d3860e8b2f6251177c8139d4cbd5b11b96361e5
xFoxie <121188705+xFoxie@users.noreply.github.com>
2024-04-25 21:59:46 -0500

    Delete games directory

--- games/header.js
+++
@@ -1,9 +0,0 @@
-// header.js
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        document.head.innerHTML += xhr.responseText;
-    }
-};
-xhr.open('GET', 'header.html', true);
-xhr.send();
